716c7b993b545b34f0733d11c0911f00c0a28728
Shadab Ali <shadabali604@gmail.com>
2020-10-22 22:08:48 +0530

    added: fetch method is not fully completed

--- 03 - Promise/02 - fetch/fetch.js
+++ 03 - Promise/02 - fetch/fetch.js
@@ -3,7 +3,19 @@
 // 2. Returns a promise
 // 3. Resolve the promise when data is fetched (onload)
 // 4. Reject the promise when error occured (onerror)
-function fetchData() {
+function fetchData(url) {
   // your code goes here
-  return new promise()
+return new Promise((resolve, reject) => {
+    let xhr = new XMLHttpRequest();
+     xhr.onload  = resolve();
+     xhr.onabort = reject(e = console.log(e))
+  
+  xhr.open('GET', url)
+  xhr.send();
+})
 }
+
+
+
+fetchData('https://api.github.com/users/shadab-me');
+console.log(fetchData())
