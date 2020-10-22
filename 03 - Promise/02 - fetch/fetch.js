e9fb1e037f913c4a6f35618a8c5a54b7f5470cd8
Shadab Ali <shadabali604@gmail.com>
2020-10-22 22:48:26 +0530

    added: fetch method impleted

--- 03 - Promise/02 - fetch/fetch.js
+++ 03 - Promise/02 - fetch/fetch.js
@@ -7,15 +7,19 @@ function fetchData(url) {
   // your code goes here
 return new Promise((resolve, reject) => {
     let xhr = new XMLHttpRequest();
-     xhr.onload  = resolve();
-     xhr.onabort = reject(e = console.log(e))
+    xhr.open('GET', url);
+     xhr.onload  =  () => {
+        return resolve(JSON.parse(xhr.response));
+     };
+     xhr.onabort  = () => {
+      return reject(e => console.log(e))
+     }
   
-  xhr.open('GET', url)
   xhr.send();
 })
 }
 
 
 
-fetchData('https://api.github.com/users/shadab-me');
-console.log(fetchData())
+console.log(fetchData('https://api.github.com/users/shadab-me'));
+ 
